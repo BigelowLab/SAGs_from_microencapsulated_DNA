c04f2d8acf3df851d791bc26f5ee292caf2a8a87
Greg Gavelis <43386190+ggavelis@users.noreply.github.com>
2026-09-08 13:49:15 -0400

    Remove email notification from assemble.pbs.sh
    Removed email notification setting from PBS script.

--- assemble.pbs.sh
+++ assemble.pbs.sh
@@ -1,12 +1,11 @@
 #PBS -N spc_github
-#PBS -M ggavelis@bigelow.org
 #PBS -m bea -j oe -o status_of_assembly.pbs
 #PBS -l ncpus=1,mem=50MB,walltime=120:00:00
 module purge
 module load singularity
 module load nextflow
 cd /mnt/scgc/scgc_temp/spc_github/
-NXF_VER=25.04.6 nextflow run /mnt/scgc/scgc_temp/spc_github/main.nf -profile charlie -resume \
+nextflow run /mnt/scgc/scgc_temp/spc_github/main.nf -profile charlie -resume \
   -name SAGs_from_microcapsules_$(date +%Y%m%d_%H%M%S) \
   --contam_ref_fasta /mnt/scgc_nfs/ref/assembly_protocol/GRCh38_AG665_mm10.fa --viral
 
